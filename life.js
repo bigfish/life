9720910c2f7259a46302f5358ddcfd69333eea40
David Wilhelm <dewilhelm@gmail.com>
2011-08-22 01:04:15 -0400

    got loading clicked on thumbnails working, except when scrolled up

--- life.js
+++ life.js
@@ -1,7 +1,7 @@
 /*jslint evil:true */
 /*globals METADATA */
 (function () {
-    var canvas, t_canvas, ctx, fg, bg, size, timer, rows, cols, cells, oldCells, width, height, patterns;
+    var canvas, t_canvas, ctx, fg, bg, size, timer, rows, cols, cells, oldCells, width, height, patterns, makeThumbnails;
 
     function clear() {
         ctx.fillStyle = bg;
@@ -117,119 +117,7 @@
         render();
     }
 
-    function makeThumbnails(t_width, t_height) {
-        var p, pattern, c, r, scale_x, scale_y, rows, canvasEl = document.getElementById("thumbnails"),
-            ctx = canvasEl.getContext("2d"),
-            numPatterns = patterns.length,
-            space = 5,
-            //determine how many cols can be fit in the available space
-            //TODO: consider if not enough space is available for even one
-            //the game area should be reduced to allow size for it
-            t_cols = Math.floor((window.innerWidth - 600) / (t_width + space)) || 1,
-            t_rows = Math.ceil(patterns.length / t_cols),
-            c_width = t_cols * (t_width + space),
-            c_height = t_rows * (t_height + space);
 
-
-        t_canvas = canvasEl;
-
-        canvasEl.setAttribute("width", c_width + "px");
-        //calculate height required
-        canvasEl.setAttribute("height", c_height + "px");
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, c_width, c_height);
-        ctx.fillStyle = "#FFF";
-        for (p = 0; p < patterns.length; p++) {
-            pattern = patterns[p];
-            scale_x = t_width / pattern.cols;
-            scale_y = t_height / pattern.rows;
-            //preserve aspect ratio
-            if (scale_y > scale_x) {
-                scale_y = scale_x;
-            } else {
-                scale_x = scale_y;
-            }
-            rows = pattern.data.split("\n");
-            for (r = 0; r < pattern.rows; r++) {
-                for (c = 0; c < pattern.cols; c++) {
-                    if (rows[r].charAt(c) === "O") {
-                        ctx.fillRect((Math.floor(p / t_rows) * (t_width + space)) + Math.floor(c * scale_x), Math.floor((p % t_rows) * (t_height + space) + r * scale_y), scale_x, scale_y);
-                    }
-                }
-            }
-        }
-    }
-
-    function loadPatterns(menu, textArea) {
-        var patterns_img = new Image(),
-            metadata_img = new Image(),
-            canvas = document.createElement('canvas'),
-            ctx = canvas.getContext("2d"),
-            patterns_data = [],
-            patterns_data_text = "",
-            pattern_str = "",
-            patterns_metadata = [],
-            patterns_metadata_text = "";
-        patterns = [];
-        patterns_img.onload = function () {
-            var i, o, imagedata, rows, cols, r, c, offset, options = [],
-                width = patterns_img.width,
-                height = patterns_img.height;
-            canvas.setAttribute("width", width + "px");
-            canvas.setAttribute("height", height + "px");
-            ctx.drawImage(patterns_img, 0, 0, width, height);
-            imagedata = ctx.getImageData(0, 0, width, height).data;
-            for (i = 0; i < imagedata.length; i += 4) {
-                patterns_data.push(imagedata[i] ? '.' : 'O');
-            }
-            patterns_data_text = patterns_data.join("");
-            //load metadata
-            metadata_img.onload = function () {
-                width = metadata_img.width;
-                height = metadata_img.height;
-                canvas.setAttribute("width", width + "px");
-                canvas.setAttribute("height", height + "px");
-                ctx.drawImage(metadata_img, 0, 0, width, height);
-                imagedata = ctx.getImageData(0, 0, width, height).data;
-                for (i = 0; i < imagedata.length; i += 4) {
-                    patterns_metadata.push(String.fromCharCode(imagedata[i]));
-                }
-                eval(patterns_metadata.join(""));
-                //construct options for menu
-                for (o = 0; o < METADATA.length; o += 4) {
-                    pattern_str = "";
-                    offset = METADATA[o + 1];
-                    rows = METADATA[o + 2];
-                    cols = METADATA[o + 3];
-                    for (r = 0; r < rows; r++) {
-                        pattern_str += patterns_data_text.substring(offset + r * cols, offset + (r + 1) * cols);
-                        if (r < rows - 1) {
-                            pattern_str += "\n";
-                        }
-
-                    }
-                    options.push("<option value='" + pattern_str + "' >" + METADATA[o] + "</option>");
-                    //add to patterns collection
-                    patterns.push({
-                        name: METADATA[o],
-                        rows: rows,
-                        cols: cols,
-                        data: pattern_str
-                    });
-                }
-                menu.innerHTML = options.join("");
-                menu.onchange = function () {
-                    textArea.value = menu.options[menu.selectedIndex].value;
-                };
-                makeThumbnails(100, 100);
-            };
-            metadata_img.src = "parser/metadata_o.png";
-        };
-        patterns_img.onerror = function () {
-            console.log("failed to load patterns data image");
-        };
-        patterns_img.src = "parser/data_o.png";
-    }
 
 
     window.LIFE = {
@@ -295,7 +183,147 @@
             render();
         },
 
-        loadPatterns: loadPatterns,
+        loadPatterns: function (menu, textArea) {
+            var patterns_img = new Image(),
+                metadata_img = new Image(),
+                canvas = document.createElement('canvas'),
+                ctx = canvas.getContext("2d"),
+                patterns_data = [],
+                patterns_data_text = "",
+                pattern_str = "",
+                patterns_metadata = [],
+                patterns_metadata_text = "";
+            patterns = [];
+            var that = this;
+            patterns_img.onload = function () {
+                var i, o, imagedata, rows, cols, r, c, offset, options = [],
+                    width = patterns_img.width,
+                    height = patterns_img.height;
+                canvas.setAttribute("width", width + "px");
+                canvas.setAttribute("height", height + "px");
+                ctx.drawImage(patterns_img, 0, 0, width, height);
+                imagedata = ctx.getImageData(0, 0, width, height).data;
+                for (i = 0; i < imagedata.length; i += 4) {
+                    patterns_data.push(imagedata[i] ? '.' : 'O');
+                }
+                patterns_data_text = patterns_data.join("");
+                //load metadata
+                metadata_img.onload = function () {
+                    width = metadata_img.width;
+                    height = metadata_img.height;
+                    canvas.setAttribute("width", width + "px");
+                    canvas.setAttribute("height", height + "px");
+                    ctx.drawImage(metadata_img, 0, 0, width, height);
+                    imagedata = ctx.getImageData(0, 0, width, height).data;
+                    for (i = 0; i < imagedata.length; i += 4) {
+                        patterns_metadata.push(String.fromCharCode(imagedata[i]));
+                    }
+                    eval(patterns_metadata.join(""));
+                    //construct options for menu
+                    for (o = 0; o < METADATA.length; o += 4) {
+                        pattern_str = "";
+                        offset = METADATA[o + 1];
+                        rows = METADATA[o + 2];
+                        cols = METADATA[o + 3];
+                        for (r = 0; r < rows; r++) {
+                            pattern_str += patterns_data_text.substring(offset + r * cols, offset + (r + 1) * cols);
+                            if (r < rows - 1) {
+                                pattern_str += "\n";
+                            }
+
+                        }
+                        options.push("<option value='" + pattern_str + "' >" + METADATA[o] + "</option>");
+                        //add to patterns collection
+                        patterns.push({
+                            name: METADATA[o],
+                            rows: rows,
+                            cols: cols,
+                            data: pattern_str
+                        });
+                    }
+                    menu.innerHTML = options.join("");
+                    menu.onchange = function () {
+                        textArea.value = menu.options[menu.selectedIndex].value;
+                    };
+                    that.makeThumbnails(100, 100);
+                };
+                metadata_img.src = "parser/metadata_o.png";
+            };
+            patterns_img.onerror = function () {
+                console.log("failed to load patterns data image");
+            };
+            patterns_img.src = "parser/data_o.png";
+        },
+
+        makeThumbnails: function (t_width, t_height) {
+            var p, pattern, c, r, scale_x, scale_y, rows, canvasEl = document.getElementById("thumbnails"),
+                ctx = canvasEl.getContext("2d"),
+                numPatterns = patterns.length,
+                space = 5,
+                //determine how many cols can be fit in the available space
+                //TODO: consider if not enough space is available for even one
+                //the game area should be reduced to allow size for it
+                t_cols = Math.floor((window.innerWidth - 600) / (t_width + space)) || 1,
+                t_rows = Math.ceil(patterns.length / t_cols),
+                c_width = t_cols * (t_width + space),
+                c_height = t_rows * (t_height + space);
+
+
+            t_canvas = canvasEl;
+
+            canvasEl.setAttribute("width", c_width + "px");
+            //calculate height required
+            canvasEl.setAttribute("height", c_height + "px");
+            ctx.fillStyle = "#000";
+            ctx.fillRect(0, 0, c_width, c_height);
+            ctx.fillStyle = "#FFF";
+            for (p = 0; p < patterns.length; p++) {
+                pattern = patterns[p];
+                scale_x = t_width / pattern.cols;
+                scale_y = t_height / pattern.rows;
+                //preserve aspect ratio
+                if (scale_y > scale_x) {
+                    scale_y = scale_x;
+                } else {
+                    scale_x = scale_y;
+                }
+                rows = pattern.data.split("\n");
+                for (r = 0; r < pattern.rows; r++) {
+                    for (c = 0; c < pattern.cols; c++) {
+                        if (rows[r].charAt(c) === "O") {
+                            ctx.fillRect((Math.floor(p / t_rows) * (t_width + space)) + Math.floor(c * scale_x), Math.floor((p % t_rows) * (t_height + space) + r * scale_y), scale_x, scale_y);
+                        }
+                    }
+                }
+            }
+
+            var that = this;
+
+            function processClick(event) {
+                var node;
+                var canvas = document.getElementsByTagName('canvas')[0];
+                var context = canvas.getContext('2d');
+                var x = event.clientX;
+                var y = event.clientY;
+                while (node) {
+                    x -= node.offsetLeft - node.scrollLeft;
+                    y -= node.offsetTop - node.scrollTop;
+                    node = node.offsetParent;
+                }
+                var relX = x - 600;
+                var col = Math.floor(relX / (t_width + space));
+                var row = Math.floor(y / (t_height + space));
+                var idx = col * t_rows + row;
+                console.log(idx);
+                if (idx < patterns.length) {
+                    that.insert_seed(patterns[idx].data);
+                }
+
+            }
+
+            t_canvas.addEventListener('click', processClick, false);
+        },
+
 
         saveThumbnails: function () {
             document.write("<img src='" + t_canvas.toDataURL("image/png") + "' >");
