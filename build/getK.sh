41b40e29468ec2194cfa8a3f21572036e3cd2641
David Wilhelm <dewilhelm@gmail.com>
2011-08-28 01:40:09 -0400

    building all files in makefile and showing total size

--- build/getK.sh
+++ build/getK.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-echo -n "Total size of pattern data: "
-find . -name "*_o.png" | xargs ls -la | awk '{sum += $5} END {print sum/1024 }'
+echo "******** SIZE REPORT ********"
+loaderSize=`ls -la loader_ugly.js | awk '{print $5}'`
+htmlSize=`ls -la ../index.html | awk '{ print $5 }'`
+#pngSize=`find . -name "*_o.png" | xargs ls -la | awk '{sum += $5} END {print sum}'`
+dataSize=`ls -la data_o.png | awk '{print $5}'`
+metaDataSize=`ls -la metadata_o.png | awk '{print $5}'`
+lifeSize=`ls -la life_o.png | awk '{print $5}'`
+echo "index.html: ${htmlSize}"
+echo "loader_ugly.js: $loaderSize"
+echo "data png: $dataSize"
+echo "metadata png: $metaDataSize"
+echo "life png: $lifeSize"
+total=`echo "($htmlSize+$loaderSize+$dataSize+$metaDataSize+$lifeSize)/1024" | bc -l`
+echo -n "******** TOTAL: ${total:0:5}K ******** "
+
 
